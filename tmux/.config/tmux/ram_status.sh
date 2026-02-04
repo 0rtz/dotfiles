605543d4c4d4f14f483f53db76faee43968efae6
0rtz <46892933+0rtz@users.noreply.github.com>
2026-02-04 16:39:34 +0300

    Refactor tmux config

--- tmux/.config/tmux/ram_status.sh
+++ tmux/.config/tmux/ram_status.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-ram_available ()
-{
+ram_available () {
 	ALL=$(free --total --human | awk -e '$1 ~ /:/ && FNR == 4 {print $2}')
 	USED=$(free --total --human | awk -e '$1 ~ /:/ && FNR == 4 {print $3}')
 	echo "${USED}/${ALL}"
